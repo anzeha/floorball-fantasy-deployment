7da291cb2564053fd22905277ba4a0f256a75e73
Anže <anze603@gmail.com>
2024-08-06 13:43:45 +0200

    Added ingress

--- gke/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "kubernetes_cluster_name" {
-  value = module.gke.name
-}
-
-output "region" {
-  value = module.gke.region
-}
--- gke/router.tf
+++
@@ -1,7 +0,0 @@
-resource "google_compute_router" "dev-router" {
-  name       = format("%s-%s-cloud-router", var.cluster_name, var.env)
-  project    = var.project_id
-  region     = var.region
-  network    = module.vpc.network_name
-  depends_on = [module.vpc]
-}
